ddd4ba6a7fd572d6265d60b00c231945f63a2ac0
myakayev <myakayev@yahoo.com>
2022-02-11 23:31:29 +0200

    Lesson 2 moved into current project

---
+++ src/main/java/course/myakaev/lesson2/ThreeStars.java
@@ -0,0 +1,17 @@
+package course.myakaev.lesson2;
+
+public class ThreeStars {
+    public static void main(String[] args) {
+
+    }
+
+    public static void elementShift (int x[], int n) {
+        if (n > 0){
+            int k = x.length - 1;
+            for (int j = 0; j < k; j++){
+                k++;
+            }
+        }
+
+    }
+}
