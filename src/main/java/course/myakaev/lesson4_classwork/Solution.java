b4f74b935d539db076f837cf1711404358b61a64
o.myakayev <myakayev@yahoo.com>
2023-08-25 21:25:50 +0300

    TopTal task

--- src/main/java/course/myakaev/lesson4_classwork/Solution.java
+++ src/main/java/course/myakaev/lesson4_classwork/Solution.java
@@ -2,28 +2,28 @@ package course.myakaev.lesson4_classwork;
 
 public class Solution {
     public String solution(String T) {
+        double doubleT;
         try {
-            double doubleT = Double.parseDouble(T);
-            if (doubleT < 34.0) {
-                return "Below the temperature range";
-            }
-            if (doubleT >= 34.0 && doubleT < 35.0) {
-                return "hypothermia";
-            }
-            if (doubleT >= 35.0 && doubleT <= 37.5) {
-                return "normal";
-            }
-            if (doubleT > 37.5 && doubleT <= 40.0) {
-                return "fever";
-            }
-            if (doubleT > 40.0 && doubleT <= 42.0) {
-                return "hyperpyrexia";
-            } else {
-                return "Above the temperature range";
-            }
-
+           doubleT = Double.parseDouble(T);
         } catch (NumberFormatException e) {
             return "Wrong format of temperature";
+        }
+        if (doubleT < 34.0) {
+            return "Below the temperature range";
+        }
+        if (doubleT < 35.0) {
+            return "hypothermia";
+        }
+        if (doubleT <= 37.5) {
+            return "normal";
+        }
+        if (doubleT <= 40.0) {
+            return "fever";
+        }
+        if (doubleT <= 42.0) {
+            return "hyperpyrexia";
+        } else {
+            return "Above the temperature range";
         }
     }
 }
